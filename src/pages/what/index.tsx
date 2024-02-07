d4b02f45a44ce099deb04bd441ce8662b4067410
JOAO TARRAN <joaotarran@gmail.com>
2024-02-06 21:35:01 -0300

    What update

--- src/pages/what/index.tsx
+++ src/pages/what/index.tsx
@@ -10,9 +10,9 @@ export default function What() {
         <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="ContentViewer">
             <div className="ServiceList">
                 {tSimple.pages[2]?.services?.map((service: Service, index: number) => (
-                    <div key={index}>
+                    <div className="ServiceGroup" key={index}>
                         <h2>{service.name}</h2>
-                        <ul>
+                        <ul className="ServiceGroup_List">
                             {service.items.map((item: string, index: number) => (
                                 <li key={index}>{item}</li>
                             ))}
